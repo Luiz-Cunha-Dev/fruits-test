0533f226e67de3f73035a7ec1da49c1bf607f320
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-02-01 21:15:40 -0300

    feat: tests done

---
+++ tests/fruits.test.ts
@@ -0,0 +1,57 @@
+import supertest from "supertest";
+import app from "../src/app";
+
+const api = supertest(app);
+
+describe("POST /fruits", () => {
+
+    it('should respond with status 201', async () => {
+        const result = await api.post("/fruits").send({name: "maçã",price: 1})
+        expect(result.status).toBe(201)
+    });
+
+    it('should respond with status 409', async () => {
+        const result = await api.post("/fruits").send({name: "maçã",price: 1})
+        expect(result.status).toBe(409)
+    });
+
+    it('should respond with status 422', async () => {
+        const result = await api.post("/fruits").send({name: "maçã"})
+        expect(result.status).toBe(422)
+    });
+
+    it('should respond with status 422', async () => {
+        const result = await api.post("/fruits").send({price: 1})
+        expect(result.status).toBe(422)
+    });
+
+    it('should respond with status 201', async () => {
+        const result = await api.post("/fruits").send({name: "banana",price: 2})
+        expect(result.status).toBe(201)
+    });
+
+})
+
+describe("GET /fruits", () => {
+
+    it('should respond with status 200', async () => {
+        const result = await api.get("/fruits")
+        expect(result.status).toBe(200)
+        expect(result.body.length).toBe(2)
+    });
+
+})
+
+describe("GET /fruits/:id", () => {
+
+    it('should respond with status 200', async () => {
+        const result = await api.get("/fruits/1")
+        expect(result.status).toBe(200)
+    });
+
+    it('should respond with status 404', async () => {
+        const result = await api.get("/fruits/3")
+        expect(result.status).toBe(404)
+    });
+
+})
